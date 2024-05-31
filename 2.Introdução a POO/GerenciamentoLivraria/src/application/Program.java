a68721d16025121f51251220738dffcd2f0ed1b8
Emerson Oliveira <emerson.520@outlook.com>
2024-05-31 10:16:54 -0300

    Adicionado exercicio de gerenciamento de livraria

--- 2.Introdução a POO/GerenciamentoLivraria/src/application/Program.java
+++ 2.Introdução a POO/GerenciamentoLivraria/src/application/Program.java
@@ -1,7 +1,37 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Livro;
+
 public class Program {
 
-	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Livro livro = new Livro();
+		
+		System.out.println("Dados do Livro: ");
+		System.out.print("Título: ");
+		livro.titulo = sc.nextLine();
+		System.out.print("Autor: ");
+		livro.autor = sc.nextLine();
+		System.out.print("Preço: ");
+		livro.preco = sc.nextDouble();
+		System.out.print("Quantidade: ");
+		livro.quantidadeEmEstoque = sc.nextInt();
+		System.out.println(livro.toString());		
+		System.out.print("Informe a quantidade de livros a ser adiciona no estoque: ");
+		int quantidade = sc.nextInt();
+		livro.entradaLivroNoEstoque(quantidade);
+		System.out.println(livro.toString());
+		System.out.print("Informe a quantidade de livros a ser removida no estoque: ");
+		quantidade = sc.nextInt();
+		livro.saidaLivroNoEstoque(quantidade);
+		System.out.println(livro.toString());
+		sc.close();
+	}
 
 }
---
+++ 2.Introdução a POO/GerenciamentoLivraria/src/entities/Livro.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Livro {
+	public String titulo;
+	public String autor;
+	public double preco;
+	public int quantidadeEmEstoque;	
+	
+	public double precoTotal() {
+		return preco * quantidadeEmEstoque;
+	}
+	
+	public int entradaLivroNoEstoque(int quantidadeEmEstoque ) {
+		return this.quantidadeEmEstoque += quantidadeEmEstoque;
+	}
+	
+	public int saidaLivroNoEstoque(int quantidadeEmEstoque ) {
+		return this.quantidadeEmEstoque -= quantidadeEmEstoque;
+	}
+	
+	public String toString() {
+		return "Titulo: " +titulo +", Autor: " + autor + ", Preço R$" + String.format("%.2f", preco) +
+				", Quantidade em estoque: " + quantidadeEmEstoque + " Unidades"
+				 + "Preço: " + String.format("%.2f", precoTotal());
+	}
+
+}
