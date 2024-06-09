246c1919b6bfe77964ba5fdafa38ec08c12b9fbe
Emerson Oliveira <emerson.520@outlook.com>
2024-06-09 18:53:53 -0300

    Add: Exercicio somar 2 vetores

---
+++ 4.Comportamento de memória, arrays e listas/SomaVetores/src/SomaVetores.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SomaVetores {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		int n;
+		
+		System.out.println("Quantos valores vai ter cada vetor?");
+		n = scanner.nextInt();
+		
+		int vetorA[] = new int[n];
+		int vetorB[] = new int[n];
+		int vetorResultado[] = new int[n];
+		
+		System.out.println("Digite os valores do vetor A:");
+		for(int index=0; index<n; index+=1) {			
+			vetorA[index] = scanner.nextInt();			
+		}
+		System.out.println("Digite os valores do vetor B:");
+		for(int index=0; index<n; index+=1) {			
+			vetorB[index] = scanner.nextInt();			
+		}
+		System.out.println("Vetor resultante: ");
+		for(int index=0; index<n; index+=1) {
+			System.out.println(vetorA[index] + vetorB[index]);
+		}
+		scanner.close();
+	}
+
+}
